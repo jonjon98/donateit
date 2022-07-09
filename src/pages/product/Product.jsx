b415900614a469a0a80039ccd8ce401bc298061f
richieang002 <84786383+richieang002@users.noreply.github.com>
2022-07-10 01:55:25 +0800

    Product test update

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import './Product.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col, Form, Card, Image, Button } from 'react-bootstrap'
+import pic1 from './Images/faith-yarn-Wr0TpKqf26s-unsplash.jpg'
+import pic2 from './Images/haryo-setyadi-acn5ERAeSb4-unsplash.jpg'
+import pic3 from './Images/ryan-hoffman-6Nub980bI3I-unsplash.jpg'
+import pic4 from './Images/icon.png'
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <div class="maincontainer">
+      <Container>
+        <Row></Row>
+        <Row>
+          <Col><Image src={pic1} fluid></Image></Col>
+          <Col><Image src={pic2} fluid></Image></Col>
+          <Col><Image src={pic3} fluid></Image></Col>
+        </Row>
+        <Row className='align-items-center'>
+          <Col sm={8}>
+            Details
+          </Col>
+          <Col sm={4}>
+            <Row><Button variant='outline-secondary' style={{width:'50%', margin:'20px', marginBottom:'20px'}}>
+            <Image src={pic4} fluid width={50} style={{marginRight:'20px'}}></Image>
+            User 1
+            </Button>{' '}</Row>
+            <Button variant='danger'>34 Likes</Button>{' '}
+            <Button variant='secondary'>Interested</Button>{' '}
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
