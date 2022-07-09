2000d9ddf7d799378ee55a5de0a479d3e025f180
jonjon98 <yapzh.jon@gmail.com>
2022-07-09 11:17:00 +0800

    feat: initialises the main structure of the project by removing unwanted files and adds in the pages we need.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/home/Home.jsx'
+import Product from './pages/product/Product.jsx'
+import Login from './pages/login/Login.jsx'
+import Profile from './pages/profile/Profile.jsx'
+import Favourites from './pages/favourites/Favourites.jsx'
+import EditListing from './pages/editListing/EditListing.jsx'
+import NotFound from './pages/notFound/NotFound.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/Product' element={<Product />} />
+              <Route path='/Login' element={<Login />} />
+              <Route path='/Profile' element={<Profile />} />
+              <Route path='/Favourites' element={<Favourites />} />
+              <Route path='/EditListing' element={<EditListing />} />
+              <Route path='*' element={<NotFound />} />
+          </Routes>
+      </Router>
+    </>
   );
 }
 
