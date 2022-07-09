e64217313376d84c698e2b920bfb450ec3dddf12
jonjon98 <yapzh.jon@gmail.com>
2022-07-09 11:31:58 +0800

    feat: gets rid of the git head thing.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-=======
 import Home from './pages/home/Home.jsx'
 import Product from './pages/product/Product.jsx'
 import Login from './pages/login/Login.jsx'
@@ -12,7 +7,6 @@ import Favourites from './pages/favourites/Favourites.jsx'
 import EditListing from './pages/editListing/EditListing.jsx'
 import NotFound from './pages/notFound/NotFound.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
->>>>>>> 2000d9ddf7d799378ee55a5de0a479d3e025f180
 
 function App() {
   return (
