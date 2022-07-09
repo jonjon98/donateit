ca1db75951fd5d89fa807f42974c67f731c0db35
jonjon98 <yapzh.jon@gmail.com>
2022-07-09 12:50:59 +0800

    feat: adds listing and recycling page.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Home from './pages/home/Home.jsx'
+import Listing from './pages/listing/Listing.jsx'
 import Product from './pages/product/Product.jsx'
 import Login from './pages/login/Login.jsx'
 import Signup from './pages/signup/Signup.jsx'
 import Profile from './pages/profile/Profile.jsx'
+import Recycling from './pages/recycling/Recycling.jsx'
 import Favourites from './pages/favourites/Favourites.jsx'
 import EditListing from './pages/editListing/EditListing.jsx'
 import NotFound from './pages/notFound/NotFound.jsx'
@@ -14,10 +16,12 @@ function App() {
       <Router>
           <Routes>
               <Route path='/' element={<Home />} />
+              <Route path='/Listing' element={<Listing />} />
               <Route path='/Product' element={<Product />} />
               <Route path='/Login' element={<Login />} />
               <Route path='/Signup' element={<Signup />} />
               <Route path='/Profile' element={<Profile />} />
+              <Route path='/Recycling' element={<Recycling />} />
               <Route path='/Favourites' element={<Favourites />} />
               <Route path='/EditListing' element={<EditListing />} />
               <Route path='*' element={<NotFound />} />
